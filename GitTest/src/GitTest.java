53e954d81c66ee2593f81cf530c4404fdd903f22
Surface <Surface@SurfacePro3MD>
2018-07-29 11:12:30 +0430

    first commit
    uploading all the files

---
+++ GitTest/src/GitTest.java
@@ -0,0 +1,22 @@
+
+public class GitTest {
+	
+	public int add (int num1, int num2)
+	{
+		int r = num1+num2;
+		return r;
+	}
+	
+	public int sub( int num1,int num2)
+	{
+		int r = num1-num2;
+		return r;
+		
+	}
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
